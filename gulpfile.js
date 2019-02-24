a26381d5d6162c1d79c8a417d9c6db9b52d7c8d2
VladimirZhdanov <VladimirZhdanov1992@gmail.com>
2019-02-25 00:43:30 +0600

    Init

---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+const gulp = require("gulp");
+const debug = require("gulp-debug");
+const sass = require("gulp-sass");
+const nunjucks = require("gulp-nunjucks-render");
+const browserSync = require("browser-sync").create();
+
+gulp.task("nunjucks", cb => {
+  gulp
+    .src("src/templates/*.html")
+    .pipe(debug({ title: "nunjucks templates:" }))
+    .pipe(
+      nunjucks({
+        path: [
+          "src/templates/chunks",
+          "src/templates/chunks/promo",
+          "src/templates/layout"
+        ]
+      })
+    )
+    .pipe(debug({ title: "nunjucks result:" }))
+    .pipe(gulp.dest("dist"));
+  browserSync.reload();
+  cb();
+});
+
+gulp.task("sass", () => {
+  return gulp
+    .src("./src/sass/style.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("./dist/css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("copy_images", () => {
+  return gulp
+    .src("./src/images/**/*")
+    .pipe(debug({ title: "copy images:" }))
+    .pipe(gulp.dest("./dist/images"));
+});
+
+gulp.task("copy_fonts", () => {
+  return gulp
+    .src("./src/fonts/**/*")
+    .pipe(debug({ title: "copy fonts:" }))
+    .pipe(gulp.dest("./dist/fonts"));
+});
+
+gulp.task("copy", cb => {
+  gulp.parallel("copy_fonts", "copy_images")(cb);
+});
+
+gulp.task("bs", function() {
+  browserSync.init({
+    server: {
+      baseDir: "./dist"
+    }
+  });
+
+  gulp.watch("src/sass/**/*.scss", gulp.parallel("sass"));
+  gulp.watch("src/templates/**/*.html", gulp.parallel("nunjucks"));
+});
